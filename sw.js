70c34a5ba6e461e228ae127b88352fa2fe9d493e
johnson-darko <johnsondarko365@gmail.com>
2025-09-25 15:33:14 -0500

    Updates

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ self.addEventListener('message', event => {
     event.ports[0].postMessage({ version: self.SW_APP_VERSION });
   }
 });
-const CACHE_NAME = 'gh-uni-guide-v5';
+const CACHE_NAME = 'gh-uni-guide-v6';
 self.SW_APP_VERSION = CACHE_NAME;
 const STATIC_RESOURCES = [
   '/',
